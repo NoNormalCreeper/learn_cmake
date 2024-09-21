7d8bd47c6ebebd3a4a26261000a4359545528e41
NoNormalCreeper <2560359315@qq.com>
2024-09-21 18:39:44 +0800

    add CMake files

---
+++ build/CMakeFiles/exec.dir/main.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/exec.dir/main.o: /home/rikka/learn_cmake-1/main.cpp \
+ /usr/include/stdc-predef.h /home/rikka/learn_cmake-1/src/hw.h
---
+++ build/CMakeFiles/hw.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw.dir/src/hw.o"
+  "CMakeFiles/hw.dir/src/hw.o.d"
+  "libhw.pdb"
+  "libhw.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
